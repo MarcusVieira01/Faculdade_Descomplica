98c12184b7b14c6f960ac2a555796fd4b1c1901d
Marcus Vieira <marcusvmvieira@gmail.com>
2023-01-28 11:57:08 -0300

    Inclusão de comentários

--- ADS/202301_01T_Estrutura_Dados/src/Aula02_SomaMatriz.java
+++ ADS/202301_01T_Estrutura_Dados/src/Aula02_SomaMatriz.java
@@ -1,23 +1,27 @@
+//Importação de classe externa 
 import javax.swing.JOptionPane;
 
+/**
+ * @apiNote Classe que fará a soma dos 120 elementos de uma matriz 10x12
+ */
 public class Aula02_SomaMatriz {
     public static void main(String[] args) {
-        //
+        //Declaração de variáveis
         int matriz[][] = new int[10][12];
         int soma = 0;
 
-        //
+        //Loop for encadeado que fará a atribuição de cada valor digitado pelo usuário ao elemento da matriz pertinente e o acúmulo
         for (int linha = 0; linha <= 9; linha++) {
             for (int coluna = 0; coluna <= 11; coluna++) {
-                //
+                //Atribuição do valor inserido pelo usuário ao elemento pertinente da matriz
                 matriz[linha][coluna] = Integer.parseInt(JOptionPane.
                 showInputDialog(null, "Insira um número inteiro para a posição " + 
                                                        "[" + linha + "][" + coluna + "]"));
-                //
+                //Acúmulo do valor do elemento da matriz na variável soma
                 soma += matriz[linha][coluna];
             }
         }
-        //
+        //Exibição do valor da variável soma
         JOptionPane.showMessageDialog(null, "A soma dos 120 elementos é " + soma);
     }
 }
