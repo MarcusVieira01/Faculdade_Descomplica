d7605468bb48cc451252c98ab5d773b5bad43efb
Marcus Vieira <marcusvmvieira@gmail.com>
2023-02-11 12:16:58 -0300

    Criação de arquivos com a lógica do exercício prático da aula 05

---
+++ ADS/202301_01T_Estrutura_Dados/src/Aula05_Pratica.java
@@ -0,0 +1,20 @@
+public class Aula05_Pratica {
+    public static void main(String[] args) {
+        
+    }
+
+    //
+    public int pot(int base, int exp){
+        //
+        if(base == 0){
+            //
+            return 0;
+        }else if(exp == 0){
+            //
+            return 1;
+        }
+
+
+        return 1;
+    } 
+}
